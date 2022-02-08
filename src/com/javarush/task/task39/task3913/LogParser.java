57ac00cf48a7f226cec1cfaddc84cede0c47c714
Dmitry Shishkin <4144442@bk.ru>
2022-02-08 22:21:06 +0300

    Second value

--- src/com/javarush/task/task39/task3913/LogParser.java
+++ src/com/javarush/task/task39/task3913/LogParser.java
@@ -3,147 +3,147 @@ package com.javarush.task.task39.task3913;
 import com.javarush.task.task39.task3913.query.IPQuery;
 
 import java.io.*;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class LogParser implements IPQuery {
 
     private final Path logDir;
-    private TreeMap<Date, String> logs;
+    private final Set<LogEntry> logs;
+    private final DateFormat df;
 
     public LogParser(Path logDir) {
         this.logDir = logDir;
-        initialize();
+        logs = new HashSet<>();
+        df = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        loadLogs();
     }
 
     @Override
     public int getNumberOfUniqueIPs(Date after, Date before) {
-        if (after == null)
-            after = logs.firstKey();
-        if (before == null)
-            before = logs.lastKey();
         return getUniqueIPs(after, before).size();
     }
 
     @Override
     public Set<String> getUniqueIPs(Date after, Date before) {
-        if (after == null)
-            after = logs.firstKey();
-        if (before == null)
-            before = logs.lastKey();
-        return logs.subMap(after, before)
-                .values()
-                .stream()
-                .map(this::getIp)
-                .collect(Collectors.toSet());
+        Set<String> result = new HashSet<>();
+        for (LogEntry le : logs
+        ) {
+            if (le.betweenDates(after, before))
+                result.add(le.ip);
+        }
+        return result;
     }
 
     @Override
     public Set<String> getIPsForUser(String user, Date after, Date before) {
-        if (after == null)
-            after = logs.firstKey();
-        if (before == null)
-            before = logs.lastKey();
-        return logs.subMap(after, before)
-                .values()
-                .stream()
-                .filter(l -> getUser(l).equals(user))
-                .map(this::getIp)
-                .collect(Collectors.toSet());
+        Set<String> result = new HashSet<>();
+        for (LogEntry le : logs
+        ) {
+            if (le.betweenDates(after, before) && le.name.equals(user))
+                result.add(le.ip);
+        }
+        return result;
     }
 
     @Override
     public Set<String> getIPsForEvent(Event event, Date after, Date before) {
-        if (after == null)
-            after = logs.firstKey();
-        if (before == null)
-            before = logs.lastKey();
-        return logs.subMap(after, before)
-                .values()
-                .stream()
-                .filter(l -> getEvent(l).equals(event))
-                .map(this::getIp)
-                .collect(Collectors.toSet());
+        Set<String> result = new HashSet<>();
+        for (LogEntry le : logs
+        ) {
+            if (le.betweenDates(after, before) && le.event.equals(event))
+                result.add(le.ip);
+        }
+        return result;
     }
 
     @Override
     public Set<String> getIPsForStatus(Status status, Date after, Date before) {
-        if (after == null)
-            after = logs.firstKey();
-        if (before == null)
-            before = logs.lastKey();
-        return logs.subMap(after, before)
-                .values()
-                .stream()
-                .filter(l -> getStatus(l).equals(status))
-                .map(this::getIp)
-                .collect(Collectors.toSet());
+        Set<String> result = new HashSet<>();
+        for (LogEntry le : logs
+        ) {
+            if (le.betweenDates(after, before) && le.status.equals(status))
+                result.add(le.ip);
+        }
+        return result;
     }
 
-    private void initialize() {
-        if (logs != null)
-            return;
-        logs = new TreeMap<>(Comparator.comparingLong(Date::getTime));
-        String log;
-        Set<Path> paths = null;
-        try {
-            paths = Files.list(logDir)
-                    .filter(Files::isRegularFile)
-                    .filter(f -> f.toString().toLowerCase().endsWith(".log"))
-                    .collect(Collectors.toSet());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (paths == null)
-            return;
-        for (Path path : paths
+    private void loadLogs() {
+        Set<Path> set = getLogsFiles();
+        LogEntry logEntry;
+        String status;
+        String[] parts;
+        int task = 0;
+        for (Path path : set
         ) {
             try (BufferedReader br = Files.newBufferedReader(path)) {
                 while (br.ready()) {
-                    log = br.readLine();
-                    logs.put(getDate(log), log);
+                    parts = br.readLine().split("\t");
+                    switch (parts.length) {
+                        case 5:
+                            status = parts[4];
+                            break;
+                        case 6:
+                            status = parts[5];
+                            task = Integer.parseInt(parts[4]);
+                            break;
+                        default:
+                            continue;
+                    }
+                    logEntry = new LogEntry(parts[0], parts[1], df.parse(parts[2]), Event.valueOf(parts[3])
+                            , task, Status.valueOf(status));
+                    logs.add(logEntry);
+                    task = 0;
                 }
-            } catch (IOException e) {
+            } catch (IOException | ParseException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private String getIp(String log) {
-        String[] string = log.split("\t");
-        return string[0];
-    }
-
-    private String getUser(String log) {
-        String[] string = log.split("\t");
-        return string[1];
-    }
-
-    private Event getEvent(String log) {
-        String[] string = log.split("\t");
-        return Event.valueOf(string[3]);
-    }
-
-    private Status getStatus(String log) {
-        String[] strings = log.split("\t");
-        String string = strings.length == 5 ? strings[4] : strings[5];
-        return Status.valueOf(string);
-    }
-
-    private Date getDate(String log) {
-        Date date = null;
-        DateFormat df = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-        String[] string = log.split("\t");
-        try {
-            date = df.parse(string[2]);
-        } catch (ParseException e) {
+    private Set<Path> getLogsFiles() {
+        Set<Path> result = new HashSet<>();
+        try (DirectoryStream<Path> ds = Files.newDirectoryStream(logDir)) {
+            for (Path path : ds
+            ) {
+                if (path.toString().toLowerCase().endsWith(".log"))
+                    result.add(path);
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return date;
+        return result;
+    }
+
+    private class LogEntry {
+
+        private final String ip;
+        private final String name;
+        private final Date date;
+        private final Event event;
+        private final int task;
+        private final Status status;
+
+        public LogEntry(String ip, String name, Date date, Event event, int task, Status status) {
+            this.ip = ip;
+            this.name = name;
+            this.date = date;
+            this.event = event;
+            this.task = task;
+            this.status = status;
+        }
+
+        private boolean betweenDates(Date after, Date before) {
+            if (after == null)
+                after = new Date(0L);
+            if (before == null)
+                before = new Date(Long.MAX_VALUE);
+            return date.after(after) && date.before(before);
+        }
     }
 }
--- src/com/javarush/task/task39/task3913/Solution.java
+++ src/com/javarush/task/task39/task3913/Solution.java
@@ -7,6 +7,6 @@ public class Solution {
 
     public static void main(String[] args) {
         LogParser logParser = new LogParser(Paths.get("C:\\Users\\Дмитрий\\IdeaProjects\\JavaRush\\LogParser\\src\\com\\javarush\\task\\task39\\task3913\\logs"));
-        System.out.println(logParser.getIPsForStatus(Status.OK,null, null));
+        System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
     }
 }
